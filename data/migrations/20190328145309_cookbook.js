98f4eee4cc224ac9088d675f46eae3779068e5e4
IVB107 <ianvb107@gmail.com>
2019-03-28 15:37:23 -0500

    Added tables to Cookbook db

--- data/migrations/20190328145309_cookbook.js
+++ data/migrations/20190328145309_cookbook.js
@@ -1,8 +1,78 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema
+    .createTable('dishes', table => {
+      table.increments();
+
+      table
+        .string('name', 128)
+        .notNullable()
+        .unique();
+    })
+    .createTable('recipes', table => {
+      table.increments();
+
+      table
+        .string('name', 128)
+        .notNullable()
+        .unique();
+      
+      table
+        .text('directions')
+        .notNullable();
+    })
+    .createTable('ingredients', table => {
+      table.increments();
+
+      table
+        .string('name', 255)
+        .notNullable()
+        .unique();
+    })
+    .createTable('recipe_styles', table => {
+      table.increments();
+
+      table
+        .string('name', 128)
+        .notNullable()
+        .unique();
+      
+      table
+        .integer('dish_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('dishes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+    })
+    .createTable('recipe_ingredients', table => {
+      table.increments();
+
+      table
+        .string('name', 128)
+        .notNullable()
+        .unique();
+      
+      table
+        .integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('dishes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      
+      table
+        .decimal('quantity').notNullable();
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTableIfExists('recipe_ingredients')
+    .dropTableIfExists('recipe_styles')
+    .dropTableIfExists('ingredients')  
+    .dropTableIfExists('recipes')
+    .dropTableIfExists('dishes');
 };
